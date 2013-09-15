5cd6e3f62d088bb4159394f0c7860b15e4919004
D <dannyqho@gmail.com>
2013-09-15 04:29:35 -0400

    added functionality to check if data recieved is a deta

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var io = require("socket.io").listen(app.listen(port));
 
 var buses = [];
 
+var delta = false;
+
 app.use(express.static(__dirname + '/public'));
 
 function getData() {
@@ -30,10 +32,13 @@ function parseData(data) {
     var vehicleID = data.bus[i].VehicleID;
     var matchFound = false;
 
-    for (n=0; n<data.length; n++) {
-      if (data[n].vehicleID == vehicleID) {
+
+    for (n=0; n<buses.length; n++) {
+      if (buses[n] != null &&
+            buses[n].vehicleID != null && buses[n].vehicleID == vehicleID) {
         matchFound = true;
-        // TODO: match found, have to check if delta
+        checkIfDelta(n, data.bus[i]);
+        break;
       }
     }
 
@@ -46,6 +51,7 @@ function parseData(data) {
         blockID: data.bus[i].BlockID,
         tripID: data.bus[i].TripID,
         direction: data.bus[i].Direction,
+        destination: data.bus[i].destination,
         offset: data.bus[i].Offset
       };
       buses.push(obj);
@@ -53,8 +59,61 @@ function parseData(data) {
   }
 }
 
-function getDelta() {
-
+function checkIfDelta(i, data) {
+  if (buses[i].lat != data.lat) {
+    console.log(buses[i].lat);
+    delta = true;
+    buses[i].lat = data.lat;
+    console.log(data.lat);
+  }
+  if (buses[i].lng != data.lng) {
+    console.log(buses[i].lng);
+    delta = true;
+    buses[i].lng = data.lng;
+    console.log(data.lng);
+  }
+  if (buses[i].label != data.label) {
+    console.log(buses[i].label);
+    delta = true;
+    buses[i].label = data.label;
+    console.log(data.label);
+  }
+  if (buses[i].vehicleID != data.VehicleID) {
+    console.log(buses[i].vehicleID);
+    delta = true;
+    buses[i].vehicleID = data.VehicleID;
+    console.log(data.VehicleID);
+  }
+  if (buses[i].blockID != data.BlockID) {
+    console.log(buses[i].blockID);
+    delta = true;
+    buses[i].blockID = data.BlockID;
+    console.log(data.BlockID);
+  }
+  if (buses[i].tripID != data.TripID) {
+    console.log(buses[i].tripID);
+    delta = true;
+    buses[i].tripID = data.TripID;
+    console.log(data.TripID);
+  }
+  if (buses[i].direction != data.Direction) {
+    console.log(buses[i].direction);
+    delta = true;
+    buses[i].direction = data.Direction;
+    console.log(data.Direction);
+  }
+  if (buses[i].destination != data.destination) {
+    console.log(buses[i].destination);
+    delta = true;
+    buses[i].destination = data.destination;
+    console.log(data.destination);
+  }
+  if (buses[i].offset != data.Offset) {
+    console.log(buses[i].offset);
+    delta = true;
+    buses[i].offset = data.Offset;
+    console.log(data.Offset);
+  }
 }
 
 io.sockets.on('connection', function(socket) {
@@ -67,6 +126,10 @@ function sendTimedMessage(socket) {
 
   // send deltas of the data every 2 seconds
   setInterval(function() {
-    socket.emit('delta', { message: 'test' });
+    getData();
+    if (delta == true) {
+      socket.emit('delta', { message: 'test' });
+      delta = false;
+    }
   }, 2000);
 }
